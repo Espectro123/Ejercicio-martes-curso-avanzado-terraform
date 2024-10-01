62351e88bf9108e9c3bc751ec815261fc246b31b
ivilla <ivilla@knowmadmood.com>
2024-10-01 19:05:14 +0200

    added VPC

--- main.tf
+++ main.tf
@@ -5,3 +5,18 @@ resource "aws_s3_bucket" "bucket" {
     Name = var.bucket_name
   }
 }
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.environment == "dev" ? ["us-east-2a"]: ["us-east-2a", "us-east-2b", "us-east-2c"]
+  private_subnets = [ var.private_subnets ]
+  public_subnets  = var.public_subnets
+
+  tags = {
+    Environment = var.environment
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,7 +1,33 @@
+# Global variables
+
 variable "environment" {
   type = string
 }
 
+# Bucket variables
+
 variable "bucket_name" {
   type = string
+}
+
+# VPC variables
+
+variable "vpc_name" {
+    type = string
+    description = "Name for the VPC"
+}
+
+variable "vpc_cidr" {
+    type = string
+    description = "CIDR for the VPC"
+}
+
+variable "public_subnets" {
+    type = list(string)
+    description = "Public subnets"
+}
+
+variable "private_subnets" {
+    type = string
+    description = "Private subnets"
 }
